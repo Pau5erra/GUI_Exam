49695b19a2389016b0c44842b89204b80fcabc6b
Pau5erra <pau.serra11@hotmail.com>
2015-12-11 01:33:31 +0100

    gui_exam

---
+++ Physics3D_class5 - solution/ModulePlayer.cpp
@@ -0,0 +1,182 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModulePlayer.h"
+#include "Primitive.h"
+#include "PhysVehicle3D.h"
+#include "PhysBody3D.h"
+
+ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled), vehicle(NULL)
+{
+	turn = acceleration = brake = 0.0f;
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player");
+
+	position.setX(0);
+	position.setY(12);
+	position.setZ(10);
+
+	VehicleInfo car;
+
+	// Car properties ----------------------------------------
+	car.chassis_size.Set(2, 2, 4);
+	car.chassis_offset.Set(0, 1.5, 0);
+	car.mass = 500.0f;
+	car.suspensionStiffness = 15.88f; //Lo dura que va la suspenció, cotxe de carreres pilla una pedra i es trenca
+	car.suspensionCompression = 0.83f;
+	car.suspensionDamping = 0.88f;
+	car.maxSuspensionTravelCm = 1000.0f; //Quan em puc arribar a enfonsar.
+	car.frictionSlip = 5.5; //Valors de fricció, com més alt més be es controla el cotxe, per fer que el cotxe ha de derrapar
+	car.maxSuspensionForce = 6000.0f; //Maxima força del chasis
+
+	// Wheel properties ---------------------------------------
+	float connection_height = 1.2f;
+	float wheel_radius = 0.6f;
+	float wheel_width = 0.5f;
+	float suspensionRestLength = 1.2f;
+
+	// Don't change anything below this line ------------------
+
+	float half_width = car.chassis_size.x*0.5f;
+	float half_length = car.chassis_size.z*0.5f;
+	
+	vec3 direction(0,-1,0);
+	vec3 axis(-1,0,0);
+	
+	car.num_wheels = 4;
+	car.wheels = new Wheel[4];
+
+	// FRONT-LEFT ------------------------
+	car.wheels[0].connection.Set(half_width - 0.3f * wheel_width, connection_height, half_length - wheel_radius);
+	car.wheels[0].direction = direction;
+	car.wheels[0].axis = axis;
+	car.wheels[0].suspensionRestLength = suspensionRestLength; 
+	car.wheels[0].radius = wheel_radius;
+	car.wheels[0].width = wheel_width;
+	car.wheels[0].front = true;
+	car.wheels[0].drive = true;
+	car.wheels[0].brake = false;
+	car.wheels[0].steering = true;
+
+	// FRONT-RIGHT ------------------------
+	car.wheels[1].connection.Set(-half_width + 0.3f * wheel_width, connection_height, half_length - wheel_radius);
+	car.wheels[1].direction = direction;
+	car.wheels[1].axis = axis;
+	car.wheels[1].suspensionRestLength = suspensionRestLength;
+	car.wheels[1].radius = wheel_radius;
+	car.wheels[1].width = wheel_width;
+	car.wheels[1].front = true;
+	car.wheels[1].drive = true;
+	car.wheels[1].brake = false;
+	car.wheels[1].steering = true;
+
+	// REAR-LEFT ------------------------
+	car.wheels[2].connection.Set(half_width - 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
+	car.wheels[2].direction = direction;
+	car.wheels[2].axis = axis;
+	car.wheels[2].suspensionRestLength = suspensionRestLength;
+	car.wheels[2].radius = wheel_radius;
+	car.wheels[2].width = wheel_width;
+	car.wheels[2].front = false;
+	car.wheels[2].drive = false;
+	car.wheels[2].brake = true;
+	car.wheels[2].steering = false;
+
+	// REAR-RIGHT ------------------------
+	car.wheels[3].connection.Set(-half_width + 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
+	car.wheels[3].direction = direction;
+	car.wheels[3].axis = axis;
+	car.wheels[3].suspensionRestLength = suspensionRestLength;
+	car.wheels[3].radius = wheel_radius;
+	car.wheels[3].width = wheel_width;
+	car.wheels[3].front = false;
+	car.wheels[3].drive = false;
+	car.wheels[3].brake = true;
+	car.wheels[3].steering = false;
+
+	vehicle = App->physics->AddVehicle(car);
+	vehicle->SetPos(0, 12, 10);
+
+	App->camera->VehicleToFollow(vehicle);
+	return true;
+}
+
+// Unload assets
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModulePlayer::Update(float dt)
+{
+	turn = acceleration = brake = 0.0f;
+
+	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+	{
+		acceleration = MAX_ACCELERATION;
+	}
+	
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	{
+		
+			acceleration = -MAX_ACCELERATION;
+		
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_T) == KEY_REPEAT)
+	{
+		max_turbo = 0;
+		while (max_turbo < 1000){
+			acceleration = 10000.0f;
+			max_turbo++;
+		}
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	{
+		if(turn < TURN_DEGREES)
+			turn +=  TURN_DEGREES;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	{
+		if(turn > -TURN_DEGREES)
+			turn -= TURN_DEGREES;
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_B) == KEY_REPEAT)
+	{
+		//vehicle->info.frictionSlip = 0.5;
+		//vehicle->vehicle->updateFriction(0.5f);
+		//brake = BRAKE_POWER;
+	}
+	
+	if (App->input->GetKey(SDL_SCANCODE_B) == KEY_UP)
+	{
+		vehicle->info.frictionSlip = 5.5;
+
+	}
+	vehicle->ApplyEngineForce(acceleration);
+	vehicle->Turn(turn);
+	vehicle->Brake(brake);
+
+	vehicle->Render();
+
+	char title[80];
+	sprintf_s(title, "%.1f Km/h", vehicle->GetKmh());
+	App->window->SetTitle(title);
+
+	return UPDATE_CONTINUE;
+}
+
+
+
